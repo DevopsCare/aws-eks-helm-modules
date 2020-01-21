a134b3bb0a571c472c8880cfd7b10167e77ad4fe
Pavels Veretennikovs <PVeretennikovs@riskfocus.com>
2020-01-21 17:48:12 +0200

    fix: remove no longer existing coreos helm repo

--- k8-sysconfig.tf
+++ k8-sysconfig.tf
@@ -1,8 +1,3 @@
-data "helm_repository" "coreos" {
-  name = "coreos"
-  url  = "https://s3-eu-west-1.amazonaws.com/coreos-charts/stable/"
-}
-
 module "autoscaler" {
   source       = "./modules/autoscaler"
   aws_region   = local.aws_region
