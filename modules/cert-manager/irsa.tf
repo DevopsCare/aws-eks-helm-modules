a50065e158b4acc23a23823983a55114b800b2d0
Pavels Veretennikovs <PVeretennikovs@riskfocus.com>
2020-12-08 14:36:55 +0200

    feat: terraform-aws-modules to 3.6.0

--- modules/cert-manager/irsa.tf
+++ modules/cert-manager/irsa.tf
@@ -1,10 +1,9 @@
 module "iam_assumable_role_admin" {
   source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version                       = "3.4.0"
+  version                       = "3.6.0"
   create_role                   = true
   role_name                     = "eks-${var.certmanager_release_name}"
   provider_url                  = var.irsa_provider_url
-  number_of_role_policy_arns    = 1
   role_policy_arns              = ["arn:aws:iam::aws:policy/AmazonRoute53FullAccess"]
   oidc_fully_qualified_subjects = ["system:serviceaccount:${var.certmanager_namespace}:${var.certmanager_release_name}"]
 }
